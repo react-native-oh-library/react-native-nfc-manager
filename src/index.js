bc8369f3e42c6dc0e8688bd96d200d0ab7f660d9
zy432099 <46698434+zy432099@users.noreply.github.com>
2024-12-23 09:25:17 +0800

    开源五项核查 (#19)
    Co-authored-by: 张宇 <478110507@qq.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import {Platform} from 'react-native';
 import Ndef from '../ndef-lib';
 import {NfcEvents, NfcTech, NdefStatus} from './NfcManager';
